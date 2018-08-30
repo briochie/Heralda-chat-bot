ec7ed1a2892b269972cfeb7b20d34e6997891d23
Brad Jones <bradster.jones@gmail.com>
2018-08-30 11:52:38 -0500

    Improve exit code
    I learned shortly after pushing my last bit of code that using exit() is
    bad practice, and for good reason: it obscures any errors I may have
    had.
    
    This fixes the issue.

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const Responder = require("./components/responder.js");
 
 const HeraldPlugin = require('./components/herald.js');
 
+process.on('exit', destroyOnExit);
+process.on('SIGINT', destroyOnExit);
+process.on('SIGUSR1', destroyOnExit);
+process.on('SIGUSR2', destroyOnExit);
+
 client.login(connectionConfig.token).then(() => {
     console.log("================ HERALDA ================")
     console.log("Started at: ", new Date());
     console.log("Connected as: ", client.user.tag);
     console.log("Connected at: ", new Date());
     console.log("============================");
-}).catch(() => {
-    console.error("Connection Failed");
+}).catch((err) => {
+    console.error("Connection Failed: " + err);
+    process.exitCode = 1;
 });
 
 const Herald = new HeraldPlugin(client);
 
-process.on('exit', destroyOnExit);
-process.on('SIGINT', destroyOnExit);
-process.on('SIGUSR1', destroyOnExit);
-process.on('SIGUSR2', destroyOnExit);
-
 function destroyOnExit() {
     console.log("Exiting program, destroying session.");
     client.destroy();
-    process.exit();
 }
